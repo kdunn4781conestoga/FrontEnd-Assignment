e07073555275abf1a3aa6bbf7695962f6162c03a
Kyle Dunn <101570040+kdunn4781conestoga@users.noreply.github.com>
2025-02-23 20:52:03 -0500

    Implemented input wrapper that displays label, error and loading along with input

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { REDDIT_URL } from "./consts";
+import InputWrapper from "./components/InputWrapper/InputWrapper";
 
 function App() {
   const [subRedditSearch, setSubRedditSearch] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
   const searchTimeout = useRef<number | null>(null);
 
   const searchForSubReddit = async (searchName: string) => {
-    const result = await fetch(
-      `${REDDIT_URL}/subreddits/search.json?q=${searchName}`
-    );
+    setLoading(true);
+    setError("");
+    try {
+      const result = await fetch(
+        `${REDDIT_URL}/subreddits/search.json?q=${searchName}`
+      );
 
-    if (result.ok) {
-      const content = await result.json();
+      if (result.ok) {
+        const content = await result.json();
 
-      console.log(content);
+        console.log(content);
+      } else {
+        setError(result.statusText);
+      }
+    } catch {
+      setError("An error occurred while fetching results.");
     }
+    setLoading(false);
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -44,8 +56,11 @@ function App() {
     <>
       <form onSubmit={handleSubmit}>
         <div className="searchContainer">
-          <input
+          <InputWrapper
+            label="Search"
             name="srName"
+            error={error}
+            loading={loading}
             content={subRedditSearch}
             value={subRedditSearch}
             onInput={(event) => {
