2bb82b471ce9022e8d05a0e00eebcda4d928e2ce
Kyle Dunn <101570040+kdunn4781conestoga@users.noreply.github.com>
2025-02-23 20:14:28 -0500

    Implemented input for searching for subreddits

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [subRedditSearch, setSubRedditSearch] = useState<string>("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        <div className="searchContainer">
+          <input
+            name="srName"
+            content={subRedditSearch}
+            value={subRedditSearch}
+            onInput={(event) => {
+              setSubRedditSearch(event.currentTarget.value);
+            }}
+          />
+        </div>
+        <button type="submit">Search for Posts</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
