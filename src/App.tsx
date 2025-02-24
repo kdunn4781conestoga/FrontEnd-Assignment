291828a0b2ec143c5c1a71beee2d24f546438c50
Kyle Dunn <101570040+kdunn4781conestoga@users.noreply.github.com>
2025-02-23 20:45:48 -0500

    Implemented api request to reddit for searching for reddit names

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
+import { REDDIT_URL } from "./consts";
 
 function App() {
   const [subRedditSearch, setSubRedditSearch] = useState<string>("");
+  const searchTimeout = useRef<number | null>(null);
+
+  const searchForSubReddit = async (searchName: string) => {
+    const result = await fetch(
+      `${REDDIT_URL}/subreddits/search.json?q=${searchName}`
+    );
+
+    if (result.ok) {
+      const content = await result.json();
+
+      console.log(content);
+    }
+  };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   };
+
+  useEffect(() => {
+    if (subRedditSearch) {
+      if (searchTimeout.current) {
+        clearTimeout(searchTimeout.current);
+      }
+      searchTimeout.current = window.setTimeout(() => {
+        searchForSubReddit(subRedditSearch);
+      }, 1000);
+    }
+
+    return () => {
+      if (searchTimeout.current) {
+        clearTimeout(searchTimeout.current);
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [subRedditSearch]);
 
   return (
     <>
